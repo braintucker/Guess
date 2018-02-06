09477b4e530f2c3d58eb8d4d45a18ad3ce9e1872
Brain Tucker <brian.briantucker@gmail.com>
2018-02-06 13:55:37 -0600

    class for downloading image is created

--- app/src/main/java/com/briantucker/guess/MainActivity.java
+++ app/src/main/java/com/briantucker/guess/MainActivity.java
@@ -1,14 +1,18 @@
 package com.briantucker.guess;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Random;
@@ -22,6 +26,34 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> celebNames = new ArrayList<String>();
     int pickedCeleb = 0;
 
+
+    public class DownloadImages extends  AsyncTask<String, Void, Bitmap> {
+
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+            }
+            catch (MalformedURLException e){
+                e.printStackTrace();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 
 
 
