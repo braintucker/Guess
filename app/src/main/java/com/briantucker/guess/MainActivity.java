2dc3fb219748faa67b2c1773becf9ecb2ec5bf1f
Brain Tucker <brian.briantucker@gmail.com>
2018-02-06 13:50:04 -0600

    content is now downloaded and split. URLs and names are stored in inidivdual ArrayLists

--- app/src/main/java/com/briantucker/guess/MainActivity.java
+++ app/src/main/java/com/briantucker/guess/MainActivity.java
@@ -10,9 +10,21 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> celebUrls = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    int pickedCeleb = 0;
+
+
+
+
 
 
     public class DownloadContent extends AsyncTask<String, Void, String>{
@@ -74,7 +86,34 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             result = dwnld.execute("http://www.posh24.se/kandisar").get();
-            Log.i("Content of URL", result);
+
+
+            //splitting the page at th is div so that unwanted images at the bottom aren't selected
+            String[] splitResult = result.split("<div class=\"sidebarContainer\">");
+
+
+            //splitResult[0] because it's choosing the first part of the split
+            //This one finds the URLs
+            Pattern p = Pattern.compile("src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
+
+            while(m.find()) {
+                celebUrls.add(m.group(1));
+            }
+
+            //This one finds the names
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+
+            while(m.find()) {
+                celebNames.add(m.group(1));
+            }
+
+
+            //Creating the random numeber to pick random celebs
+            Random random = new Random();
+            pickedCeleb = random.nextInt(celebUrls.size());
+
         }
         catch (ExecutionException e){
             e.printStackTrace();
