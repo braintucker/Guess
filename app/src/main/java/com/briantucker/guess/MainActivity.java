6e682d449cef347bff35da813f4e4f7105f3555d
Brain Tucker <brian.briantucker@gmail.com>
2018-02-06 13:33:22 -0600

    html from link is now logged in console

--- app/src/main/java/com/briantucker/guess/MainActivity.java
+++ app/src/main/java/com/briantucker/guess/MainActivity.java
@@ -1,10 +1,62 @@
 package com.briantucker.guess;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    public class DownloadContent extends AsyncTask<String, Void, String>{
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try{
+
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1){
+
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+                }
+
+                return result;
+            }
+            catch(Exception e){
+
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+
 
     public void celebPick(View view){
 
@@ -16,5 +68,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadContent dwnld = new DownloadContent();
+        String result = null;
+
+        try {
+            result = dwnld.execute("http://www.posh24.se/kandisar").get();
+            Log.i("Content of URL", result);
+        }
+        catch (ExecutionException e){
+            e.printStackTrace();
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
     }
 }
