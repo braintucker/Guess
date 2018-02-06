0aec60c05fd41d051a484de240e7e0842aba5d98
Brain Tucker <brian.briantucker@gmail.com>
2018-02-06 13:09:24 -0600

    added the img view and buttons with their proper constraints

--- app/src/main/java/com/briantucker/guess/MainActivity.java
+++ app/src/main/java/com/briantucker/guess/MainActivity.java
@@ -2,8 +2,15 @@ package com.briantucker.guess;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void celebPick(View view){
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
